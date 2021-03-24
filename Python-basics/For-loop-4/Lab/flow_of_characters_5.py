c0cc85281ac2122f1d5581ad531161f396c96ee2
Viktor Rumenov <viktorrumenov@abv.bg>
2021-03-25 00:04:50 +0100

    Adding for loop exercises from lab

---
+++ Python-basics/For-loop-4/Lab/flow_of_characters_5.py
@@ -0,0 +1,4 @@
+my_str = input()
+
+for i in range(len(my_str)):
+    print(my_str[i])
---
+++ Python-basics/For-loop-4/Lab/sum_of_numbers_7.py
@@ -0,0 +1,8 @@
+n = int(input())
+my_sum = 0
+
+for i in range(n):
+    num = int(input())
+    my_sum += num
+
+print(my_sum)
